7a95b7b1b2d50c8eda054a35c6d1fcceccc95f57
find-favor <1483136598@qq.com>
2025-06-17 15:38:18 +0800

    在主干中添加分支

---
+++ src/main/java/com/example/thrid_boot/newbranch.java
@@ -0,0 +1,15 @@
+package com.example.thrid_boot;
+
+public class newbranch {
+    public int id;
+    newbranch() {
+        System.out.println("newbranch");
+    }
+    newbranch(String name) {
+        System.out.println("newbranch with name: " + name);
+    }
+    newbranch(int id) {
+        this.id = id;
+        System.out.println("newbranch with id: " + id);
+    }
+}
