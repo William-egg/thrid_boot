dfd45f0fbdaf116335dae9caa252fbd4f9b91031
zhuangjiarun <zhuangjiarun@aistrong.com>
2025-06-17 10:24:18 +0800

    fist try

---
+++ src/test/java/com/example/thrid_boot/ThridBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thrid_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThridBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
